1d837a6dc42b4258f4a006889d9e7e5d2bdd5c61
Julie Spackman <spack84@byui.edu>
2024-01-19 23:29:30 -0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jsapp2-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'julieApp2';
+}
